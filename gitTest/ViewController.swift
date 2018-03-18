694acd9b8aab75979e0434457f502737b0e8b34b
Alfred <alfred@macbookdembp-2.lan>
2018-03-18 13:08:34 +0800

    我再提本地

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 //dsfsdfs
+    //11111
+    
+    //romote
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
